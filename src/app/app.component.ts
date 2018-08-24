b089f63731bd6a00dce6004b6e8ed393f407e62d
Radhika <rdkagnihotri@gmail.com>
2018-08-24 12:12:06 +0530

    stackblitz example practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,33 @@ import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   template: `
-        <div>{{title}}</div>
-        
-        <div><p>this is done with pipe</p>{{ 'MESSAGE.HELLO' | translate:param }}</div>
-        <p>this is done using directive</p>
-        <div [translate]="'MESSAGE.HELLO'" [translateParams]="{value: 'world'}">Hello</div>    `
+  <div>
+      <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      <label>
+        {{ 'HOME.SELECT' | translate }}
+        <select #langSelect (change)="translate.use(langSelect.value)">
+          <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
+        </select>
+      </label>
+    </div>
+       `
 })
 export class AppComponent {
   title = 'translateLib';
   param = {value: 'Bonjour'};
 
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService) {
       // this language will be used as a fallback when a translation isn't found in the current language
+      translate.addLangs(['en','fr']);
       translate.setDefaultLang('en');
-
        // the lang to use, if the lang isn't available, it will use the current loader to get them
-      translate.use('en');
-     /* translate.get('HELLO', {value: 'world'}).subscribe((res: string) => {
-        console.log(res);
-        //=> 'hello world'
-    });*/
-    translate.get('MESSAGE.HELLO', {value: 'world'}).subscribe((res: string) => {
-      console.log(res);
-      //=> 'hello world'
-  });
+      //translate.use('en');
+    
+      const browserLang = translate.getBrowserLang();
+      translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  
+  console.log(this.translate.currentLoader)
+  console.log(this.translate.getBrowserLang)
   }
   
   
